3e2726367fddcce2eb6352bfb1a1ce47a1c74062
Anowarul Kabir <akabir0101@gmail.com>
2020-06-30 00:36:31 -0400

    bug solved in job.sh

--- job.sh
+++ job.sh
@@ -1,7 +1,7 @@
+#!/usr/bin/sh
+
 ## this must be run from directory where run.py exists.
 ## --workdir is not used in this file.
-
-#!/usr/bin/sh
 
 #SBATCH --job-name=project_name
 #SBATCH --qos=csqos
